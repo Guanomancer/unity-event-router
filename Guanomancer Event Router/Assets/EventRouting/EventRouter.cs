99566607faed66cb47ab9a7f1fd32df3c0688aa6
Guanomancer <78472401+Guanomancer@users.noreply.github.com>
2022-02-22 15:58:30 +0100

    Would it be better with SO instead of Types ? Or should it use interfaces as tags and categories?

--- Guanomancer Event Router/Assets/EventRouting/EventRouter.cs
+++ Guanomancer Event Router/Assets/EventRouting/EventRouter.cs
@@ -7,14 +7,26 @@ namespace Guanomancer.EventRouting
     public static class EventRouter<T>
         where T : IEventData
     {
+        private static Dictionary<string, Type> _typeCache = new Dictionary<string, Type>();
+
+        private static List<IEventSubscriber> _subscribers;
+
         public static void Subscribe<ET>(IEventSubscriber subscriber)
             where ET : IEventData
             => Subscribe(typeof(ET), subscriber);
 
-        public static void Subscribe(string assemblyName, string eventTypeName, IEventSubscriber subscriber)
+        public static void Subscribe(string eventTypeName, string assemblyName, IEventSubscriber subscriber)
         {
+            if (_typeCache.ContainsKey(eventTypeName))
+            {
+                Subscribe(_typeCache[eventTypeName], subscriber);
+                return;
+            }
 
-            Subscribe(typeof(EventRouter<IEventData>), subscriber);
+            if(!AppDomain.CurrentDomain.GetTypeByName(eventTypeName, assemblyName, out Type type))
+                throw new UnityException($"Unable to subscribe to event type {eventTypeName} in assembly {assemblyName}.");
+            
+            Subscribe(type, subscriber);
         }
 
         public static void Subscribe(Type eventType, IEventSubscriber subscriber)
@@ -25,6 +37,13 @@ namespace Guanomancer.EventRouting
         public static void Unsubscribe<ET>(IEventSubscriber subscriber)
             where ET : IEventData
             => Unsubscribe(typeof(ET), subscriber);
+
+        public static void Unsubscribe(string eventTypeName, string assemblyName, IEventSubscriber subscriber)
+        {
+            if (!AppDomain.CurrentDomain.GetTypeByName(eventTypeName, assemblyName, out Type type))
+                throw new UnityException($"Unable to subscribe to event type {eventTypeName} in assembly {assemblyName}.");
+            Subscribe(type, subscriber);
+        }
 
         public static void Unsubscribe(Type eventType, IEventSubscriber subscriber)
         {
